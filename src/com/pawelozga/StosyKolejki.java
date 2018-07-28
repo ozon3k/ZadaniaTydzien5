96dc7d8b013b883985b2715d9609fe519cdbda4f
Pawel Ozga <pawel.ozga@hotmail.com>
2018-07-28 14:30:57 +0200

    asdasdasd

--- src/com/pawelozga/StosyKolejki.java
+++ src/com/pawelozga/StosyKolejki.java
@@ -1,33 +1,37 @@
 package com.pawelozga;
 
-import java.util.Stack;
+import java.util.*;
 
 public class StosyKolejki {
 
     public static void main(String[] args) {
         //zad 1 i 2
-//        Stack<Character> stos = new Stack<>();
-//        String slowo = "kobyła ma mały bok";
-//        System.out.println(slowo);
-//        slowo = slowo.replace(" ","");
-//        for (int i=0; i<slowo.length(); i++){
-//            stos.push(slowo.charAt(i));
-//        }
-//        String slowo2 = new String();
-//
-//
-//        do {
-//            slowo2+= stos.peek();
-//            stos.pop();
-//
-//
-//        } while (!stos.empty());
-//
-//        if(slowo.equals(slowo2)){
-//            System.out.println("palindrom");
-//        }else{
-//            System.out.println("nie palindrom");
-//        }
+        Stack<Character> stos = new Stack<>();
+        String slowo = "kobyła ma mały bok";
+        System.out.println(slowo);
+        slowo = slowo.replace(" ","");
+        for (int i=0; i<slowo.length(); i++){
+            stos.push(slowo.charAt(i));
+        }
+        String slowo2 = new String();
+
+
+        do {
+            slowo2+= stos.peek();
+            stos.pop();
+
+
+        } while (!stos.empty());
+
+        if(slowo.equals(slowo2)){
+            System.out.println("palindrom");
+        }else{
+            System.out.println("nie palindrom");
+        }
+
+
+
+
 
     }
 }
